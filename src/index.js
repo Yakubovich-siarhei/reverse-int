5a0e26f11fc69da8834a31bbebb39e9e41fa19a8
Yakubovich-siarhei <yakubovich.siarhei@mail.ru>
2023-08-07 12:47:20 +0300

    fix: js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-module.exports = function reverse (n) {
-    let result;
-    result = String(Math.abs(n));    
-    return  result.split('').reverse().join('')
-
-    
-}
+module.exports = function reverse(n) {
+  let result;
+  result = String(Math.abs(n));
+  return result.split("").reverse().join("");
+};
